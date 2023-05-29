c94f44acd273c5177f1b444dee7ddc51c3f63b40
kenanevni <kenanevni@gmail.com>
2023-05-29 10:01:25 +0300

    part two desc. added

--- SQLTASK09.sql
+++ SQLTASK09.sql
@@ -1,3 +1,8 @@
 --part one
 SELECT city.city, country.country from city 
 JOIN country ON city.country_id = country.country_id
+--part two
+--Write the INNER JOIN query where we can see the customer
+--table and the payment_id in the payment table and the first_name and
+--last_name names in the customer table together.
+
