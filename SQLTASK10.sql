2901907f958d8e57b79ffa42962d6efba6cc3190
kenanevni <kenanevni@gmail.com>
2023-06-04 10:08:35 +0300

    task1 added

--- SQLTASK10.sql
+++ SQLTASK10.sql
@@ -1,3 +1,5 @@
 --TASK1
 --Write the LEFT JOIN query where we can see the city and country names in
 -- the city table and the country table together.
+SELECT city.city,country.country FROM city
+LEFT JOIN  country on city.country_id = country.country_id;
