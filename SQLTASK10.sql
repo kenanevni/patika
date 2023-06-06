0175398bba9de95b10fa931f17a2cbeb5536a8f8
kenanevni <kenanevni@gmail.com>
2023-06-06 10:46:04 +0300

    task2 added

--- SQLTASK10.sql
+++ SQLTASK10.sql
@@ -8,3 +8,5 @@ LEFT JOIN  country on city.country_id = country.country_id;
 -- Write the RIGHT JOIN query where we can see the customer table and the payment_id in
 -- the payment table and the first_name and last_name names in the customer table together.
 
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM customer
+RIGHT JOIN payment on payment.customer_id = customer.customer_id;
