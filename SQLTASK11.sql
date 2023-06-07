052d3e72741d466005e8375e529af8bfd3150863
kenanevni <kenanevni@gmail.com>
2023-06-07 11:30:40 +0300

    task2  added

--- SQLTASK11.sql
+++ SQLTASK11.sql
@@ -6,3 +6,6 @@ SELECT first_name FROM customer;
 
 --task2
 --Let's sort the intersecting data for the first_name columns in the actor and customer tables.
+SELECT first_name FROM actor
+INTERSECT
+SELECT first_name FROM customer;
