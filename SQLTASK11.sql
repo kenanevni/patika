73bd390685fbf617de1afc1abff65a8974e503ba
kenanevni <kenanevni@gmail.com>
2023-06-07 11:34:12 +0300

    task3  added

--- SQLTASK11.sql
+++ SQLTASK11.sql
@@ -14,3 +14,6 @@ SELECT first_name FROM customer;
 --For the first_name columns in the actor and customer tables,
 --let's sort the data in the first table but not in the second table.
 
+SELECT first_name FROM actor
+EXCEPT
+SELECT first_name FROM customer;
