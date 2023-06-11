3527cf251c4299d0d9771118cf76d2c0b91e3d79
kenanevni <kenanevni@gmail.com>
2023-06-11 22:46:40 +0300

    task1  added

--- SQLTASKRECAP.sql
+++ SQLTASKRECAP.sql
@@ -1,2 +1,7 @@
 --task1
 --From the film table, list the 4 movies with the longest and the lowest replacenet_cost, starting with the 'K' character.
+
+SELECT title, length, replacement_cost FROM film
+WHERE title LIKE 'K%'
+ORDER BY length DESC, replacement_cost ASC
+LIMIT 4;
