27e61a9a67388aa5a665f7de578f32274ec0ca96
kenanevni <kenanevni@gmail.com>
2023-06-11 06:38:47 +0300

    task2 desc. added

--- SQLTASK12.sql
+++ SQLTASK12.sql
@@ -1,6 +1,10 @@
 --TASK1
 --In the film table, the movie length is shown in the length column.
 --How many movies are longer than the average movie length?
+
 SELECT COUNT(length) FROM film
 WHERE length > ALL
 (SELECT AVG(length) from film);
+
+--TASK2
+--How many movies have the highest rental_rate in the film table?
