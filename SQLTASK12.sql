817f4a77b14c4803ec7a052ae1d23b14d2aad3d6
kenanevni <kenanevni@gmail.com>
2023-06-11 06:36:12 +0300

    task1 added

--- SQLTASK12.sql
+++ SQLTASK12.sql
@@ -1,3 +1,6 @@
 --TASK1
 --In the film table, the movie length is shown in the length column.
 --How many movies are longer than the average movie length?
+SELECT COUNT(length) FROM film
+WHERE length > ALL
+(SELECT AVG(length) from film);
