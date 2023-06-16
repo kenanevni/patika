39a1111c61255b08fbbe0e05ef4424e540f47e67
kenanevni <kenanevni@gmail.com>
2023-06-16 13:51:47 +0300

    added Scanner

--- java101/src/Giris/ExamResult.java
+++ java101/src/Giris/ExamResult.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class ExamResult {
     public static void main (String [] args){
         System.out.println("Welcome To Exam Result ");
-        double math, physic, chemistry,english , history, music;
+        //create Scanner for input
+        Scanner input = new Scanner(System.in);
+
 
 
     }
