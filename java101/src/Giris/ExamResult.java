a2efa712386ff7888a93b2c0b3c0e4b94b63bbd8
kenanevni <kenanevni@gmail.com>
2023-06-16 11:40:20 +0300

    first commit for exam result app

---
+++ java101/src/Giris/ExamResult.java
@@ -0,0 +1,11 @@
+package Giris;
+import java.util.Scanner;
+
+public class ExamResult {
+    public static void main (String [] args){
+        System.out.println("Welcome To Exam Result ");
+        double math, physic, chemistry,english , history, music;
+
+
+    }
+}
