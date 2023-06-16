d2886363ac8e112dea9ce422ade10ff143f3e325
kenanevni <kenanevni@gmail.com>
2023-06-16 23:49:36 +0300

    for double round used String.format(%.2f, num)

--- java101/src/Entrance/VatCalc.java
+++ java101/src/Entrance/VatCalc.java
@@ -1,5 +1,28 @@
 package Entrance;
+
 import java.util.Scanner;
 
 public class VatCalc {
+    public static void main(String[] args) {
+        /* VAT Amount Calculator Program
+Write a program that calculates the VAT value and the VAT amount of the money value
+received from the user with Java and prints it on the screen.
+(Note: Take the VAT amount as 18%)
+Price without VAT = 10;
+
+Price with VAT = 11.8;
+
+VAT amount = 1.8;
+        */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter price");
+        double priceWithOutTax = input.nextDouble();
+        double vatRate1 = 1.18;
+        double priceWithTax = priceWithOutTax * vatRate1;
+        double vatAmount = priceWithTax-priceWithOutTax;
+        System.out.println("priceWithOutTax = " + priceWithOutTax);
+        System.out.println("priceWithTax = " + String.format("%.2f",priceWithTax));
+        System.out.println("vatAmount = " + String.format("%.2f", vatAmount));
+
+    }
 }
