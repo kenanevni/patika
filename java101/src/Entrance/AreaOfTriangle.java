6d9f30d7df2cfeda4fc2df955035d7301a4e3751
kenanevni <kenanevni@gmail.com>
2023-06-17 06:50:44 +0300

    create scanner

--- java101/src/Entrance/AreaOfTriangle.java
+++ java101/src/Entrance/AreaOfTriangle.java
@@ -1,4 +1,11 @@
 package Entrance;
 
+import java.util.Scanner;
+
 public class AreaOfTriangle {
+    public static void main(String[] args) {
+        //create Scanner
+        Scanner input = new Scanner(System.in);
+
+    }
 }
