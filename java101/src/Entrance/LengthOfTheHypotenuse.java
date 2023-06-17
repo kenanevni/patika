4aa38e7a8cc2cdcf2aa039c229deaf6f35d0f8c4
kenanevni <kenanevni@gmail.com>
2023-06-17 06:17:31 +0300

    used Math class thats all

--- java101/src/Entrance/LengthOfTheHypotenuse.java
+++ java101/src/Entrance/LengthOfTheHypotenuse.java
@@ -1,8 +1,15 @@
 package Entrance;
-import java.util.Scanner;
-public class LengthOfTheHypotenuse {
-    public static void main (Strings[] args){
-        Scanner input = new Scanner(System.in);
 
+import java.util.Scanner;
+
+public class LengthOfTheHypotenuse {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double sideA, sideB, hypotenuse;
+        System.out.println("Welcome, Please enter sides of the right triangle");
+        sideA = input.nextDouble();
+        sideB = input.nextDouble();
+        hypotenuse = Math.sqrt((sideA * sideA) + (sideB * sideB));
+        System.out.println("hypotenuse = " + hypotenuse);
     }
 }
