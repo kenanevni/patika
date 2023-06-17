307ba0251da728eb90ecd53215d8767cf04863e0
kenanevni <kenanevni@gmail.com>
2023-06-17 06:53:52 +0300

    define variables

--- java101/src/Entrance/AreaOfTriangle.java
+++ java101/src/Entrance/AreaOfTriangle.java
@@ -6,6 +6,10 @@ public class AreaOfTriangle {
     public static void main(String[] args) {
         //create Scanner
         Scanner input = new Scanner(System.in);
+        //define variables
+        int sideA, sideB, sideC, halfPerimeter;
+        double area;
+
 
     }
 }
