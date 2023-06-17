b789de35aeb952d95bb2097340877f68ec94a1d1
kenanevni <kenanevni@gmail.com>
2023-06-17 05:48:01 +0300

    Scanner imported

--- java101/src/Entrance/LengthOfTheHypotenuse.java
+++ java101/src/Entrance/LengthOfTheHypotenuse.java
@@ -2,6 +2,7 @@ package Entrance;
 import java.util.Scanner;
 public class LengthOfTheHypotenuse {
     public static void main (Strings[] args){
+        Scanner input = new Scanner(System.in);
 
     }
 }
