3469d65ea970f96554407b4d25cfb2600ab4220e
kenanevni <kenanevni@gmail.com>
2023-06-18 10:27:05 +0300

    Weight  variables  added

--- java101/src/Entrance/GroceryCashier.java
+++ java101/src/Entrance/GroceryCashier.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class GroceryCashier {
     public static void main(String[] args) {
         Scanner inout = new Scanner(System.in);
-        double pearPrice,applePrice,tomatoPrice,bananaPrice, eggplantPrice;
+        double pearPrice,applePrice,tomatoPrice,bananaPrice, eggplantPrice,
+                pearWeight, appleWeight, tomatoWeight,bananaWeight,eggPlantWeight ;
+
         pearPrice = 2.14;
         applePrice = 3.67;
         tomatoPrice = 1.11;
