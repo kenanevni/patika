c9a666a196cb1123c83cdd98891c7198ae52e51b
kenanevni <kenanevni@gmail.com>
2023-06-18 09:32:52 +0300

    impoerted Scanner

--- java101/src/Entrance/AreaOfTheSectorTheCircle.java
+++ java101/src/Entrance/AreaOfTheSectorTheCircle.java
@@ -1,4 +1,7 @@
 package Entrance;
-
+import java.util.Scanner;
 public class AreaOfTheSectorTheCircle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
 }
