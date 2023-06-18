69c72b7743480c97ffeb388ecfd7908456634a06
kenanevni <kenanevni@gmail.com>
2023-06-18 10:24:10 +0300

    price variables and values added

--- java101/src/Entrance/GroceryCashier.java
+++ java101/src/Entrance/GroceryCashier.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class GroceryCashier {
     public static void main(String[] args) {
         Scanner inout = new Scanner(System.in);
+        double pearPrice,applePrice,tomatoPrice,bananaPrice, eggplantPrice;
+        pearPrice = 2.14;
+        applePrice = 3.67;
+        tomatoPrice = 1.11;
+        bananaPrice = 0.95;
+        eggplantPrice = 5;
     }
 }
