865f78d46f0fca36b3d342eba6dc96c95b0b0a63
kenanevni <kenanevni@gmail.com>
2023-06-18 09:36:28 +0300

    added variables

--- java101/src/Entrance/AreaOfTheSectorTheCircle.java
+++ java101/src/Entrance/AreaOfTheSectorTheCircle.java
@@ -1,7 +1,11 @@
+//Write a program that finds the area of a sector of circle with radius
+// r and measure 𝛼 of the central angle.
 package Entrance;
 import java.util.Scanner;
 public class AreaOfTheSectorTheCircle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double angle,radius,area;
+        double pi = 3.14;
     }
 }
