bf4634ef4ade17b36fd704b02aa2a4141c211079
kenanevni <kenanevni@gmail.com>
2023-06-18 07:32:01 +0300

    added explanation

--- java101/src/Entrance/TaxiMeter.java
+++ java101/src/Entrance/TaxiMeter.java
@@ -1,3 +1,10 @@
+//Taximeter Program
+//        Write a program that prints the taximeter amount to the screen according to the distance traveled (KM) with Java.
+//
+//        Taximeter costs 2.20 TL per km.
+//        The minimum amount to be paid is 20 TL. For wages under 20 TL, 20 TL will be charged.
+//        The taximeter opening fee is 10 TL.
+
 package Entrance;
 
 public class TaxiMeter {
