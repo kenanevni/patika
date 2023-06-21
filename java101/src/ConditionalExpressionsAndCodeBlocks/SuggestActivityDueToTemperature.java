f9203d8da4fb150b18f826094605517e1f8e9e1d
kenanevni <kenanevni@gmail.com>
2023-06-21 21:17:10 +0300

    personal solution is added

--- java101/src/ConditionalExpressionsAndCodeBlocks/SuggestActivityDueToTemperature.java
+++ java101/src/ConditionalExpressionsAndCodeBlocks/SuggestActivityDueToTemperature.java
@@ -1,9 +1,3 @@
-/*
-Suggest to "Ski" if the temperature is less than 5.
-Suggest the "Cinema" event if the temperature is between 5 and 15.
-Suggest the "Picnic" event if the temperature is between 10 and 25.
-Suggest the "Swimming" event if the temperature is greater than 25.
-*/
 package ConditionalExpressionsAndCodeBlocks;
 
 import java.util.Scanner;
@@ -11,8 +5,8 @@ import java.util.Scanner;
 public class SuggestActivityDueToTemperature {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int temperature = input.nextInt();
         System.out.println("Please enter the weather temperature");
+        int temperature = input.nextInt();
     /*    if (temperature < 5) {
             System.out.println("Go to Ski");            
         } else if (temperature<25) {
@@ -25,6 +19,26 @@ public class SuggestActivityDueToTemperature {
         } else {
             System.out.println("Go to Swimming");
         }*/
+        if (temperature < 5) {
+            System.out.println("Go to Ski");
+        } else if (temperature <= 10) {
+            System.out.println("Go to Cinema");
+        } else if (temperature > 10 && temperature <= 15) {
+            System.out.println("Go to Cinema or Picnic");
+        } else if (temperature <= 25) {
+
+            System.out.println("Go to Picnic");
+        } else {
+            System.out.println("Go To Swimming");
+
+        }
 
     }
 }
+
+/*
+Suggest to "Ski" if the temperature is less than 5.
+Suggest the "Cinema" event if the temperature is between 5 and 15.
+Suggest the "Picnic" event if the temperature is between 10 and 25.
+Suggest the "Swimming" event if the temperature is greater than 25.
+*/
