1f256f4c5bd74e46e26cd7064512f8c9fee2f6c6
kenanevni <kenanevni@gmail.com>
2023-06-21 19:12:07 +0300

    Scanner imported

--- java101/src/ConditionalExpressionsAndCodeBlocks/SuggestActivityDueToTemperature.java
+++ java101/src/ConditionalExpressionsAndCodeBlocks/SuggestActivityDueToTemperature.java
@@ -5,6 +5,9 @@ Suggest the "Picnic" event if the temperature is between 15 and 25.
 Suggest the "Swimming" event if the temperature is greater than 25.
 */
 package ConditionalExpressionsAndCodeBlocks;
-
+import java.util.Scanner;
 public class SuggestActivityDueToTemperature {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+    }
 }
