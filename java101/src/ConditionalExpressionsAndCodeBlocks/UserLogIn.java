e148d6e6809c9022b93eeb7edef6ceca0eaf6455
kenanevni <kenanevni@gmail.com>
2023-06-21 06:08:41 +0300

    Scanner imported

---
+++ java101/src/ConditionalExpressionsAndCodeBlocks/UserLogIn.java
@@ -0,0 +1,8 @@
+package ConditionalExpressionsAndCodeBlocks;
+import java.util.Scanner;
+
+public class UserLogIn {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+    }
+}
