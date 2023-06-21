626f2a801ba4cc2630a28f6b7a2f4d43df87e85a
kenanevni <kenanevni@gmail.com>
2023-06-21 14:45:00 +0300

    boundary check

--- java101/src/ConditionalExpressionsAndCodeBlocks/ExamResult.java
+++ java101/src/ConditionalExpressionsAndCodeBlocks/ExamResult.java
@@ -8,6 +8,16 @@ public class ExamResult {
         double math, physics, chem, eng, hist, mus, avg, total = 0;
         //create Scanner for input
         Scanner input = new Scanner(System.in);
+        //Lets get lesson points from user
+        System.out.println("Please enter your math point : ");
+        math = input.nextDouble();
+        // this conditioun will check boundaries
+        // if ((math >= 0) && (math <= 100)) {
+        //            total += math;
+        //        }
+        if ((math >= 0) && (math <= 100)) {
+            total += math;
+        }
 
 
     }
