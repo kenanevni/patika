d92a4f53bd0a1cf13f08440fc94faa6d2fd73c08
kenanevni <kenanevni@gmail.com>
2023-06-21 14:42:36 +0300

    Scanner imported variables defined

--- java101/src/ConditionalExpressionsAndCodeBlocks/ExamResult.java
+++ java101/src/ConditionalExpressionsAndCodeBlocks/ExamResult.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class ExamResult {
     public static void main(String[] args) {
+        System.out.println("Welcome To Exam Result ");
+        double math, physics, chem, eng, hist, mus, avg, total = 0;
+        //create Scanner for input
+        Scanner input = new Scanner(System.in);
 
 
     }
