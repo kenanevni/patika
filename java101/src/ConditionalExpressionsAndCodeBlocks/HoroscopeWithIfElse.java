cbc668c085122a1bdbab56653c0563b39b956900
kenanevni <kenanevni@gmail.com>
2023-06-23 05:26:55 +0300

    initial commit

--- java101/src/ConditionalExpressionsAndCodeBlocks/HoroscopeWithIfElse.java
+++ java101/src/ConditionalExpressionsAndCodeBlocks/HoroscopeWithIfElse.java
@@ -1,4 +1,7 @@
 package ConditionalExpressionsAndCodeBlocks;
-
+import java.util.Scanner;
 public class HoroscopeWithIfElse {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
 }
