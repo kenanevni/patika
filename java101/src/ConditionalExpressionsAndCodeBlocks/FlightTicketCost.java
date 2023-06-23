5fb849845174afbc1caf1288a794525ddd082356
kenanevni <kenanevni@gmail.com>
2023-06-23 10:20:05 +0300

    Scanner imported

--- java101/src/ConditionalExpressionsAndCodeBlocks/FlightTicketCost.java
+++ java101/src/ConditionalExpressionsAndCodeBlocks/FlightTicketCost.java
@@ -1,4 +1,8 @@
 package ConditionalExpressionsAndCodeBlocks;
-
+import java.util.Scanner;
 public class FlightTicketCost {
+    public static vois main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
 }
+
