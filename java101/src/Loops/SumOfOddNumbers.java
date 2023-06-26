5c5cbdf75ec8641dad1661d1f6fc7791479e88d4
kenanevni <kenanevni@gmail.com>
2023-06-26 14:11:17 +0300

    complted

--- java101/src/Loops/SumOfOddNumbers.java
+++ java101/src/Loops/SumOfOddNumbers.java
@@ -2,6 +2,7 @@ package Loops;
 /*We write a program that accepts input from the user until a negative value is entered and adds the odd numbers from the entered values and prints it on the screen.*/
 
 import java.util.Scanner;
+
 public class SumOfOddNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -9,6 +10,10 @@ public class SumOfOddNumbers {
         int total = 0;//default value  0 ineffective element
         do {
             n = scan.nextInt();
-        }while (n>0)
+            if (n % 2 != 0) { // if n is odd adding total
+                total += n;
+            }
+        } while (n > 0);
+        System.out.println("total = " + total);
     }
 }
