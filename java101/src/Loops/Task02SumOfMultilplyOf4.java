e3e794adb4229e0db82f72a68ad9f35e8d7a8308
kenanevni <kenanevni@gmail.com>
2023-06-26 14:15:38 +0300

    initial commit

---
+++ java101/src/Loops/Task02SumOfMultilplyOf4.java
@@ -0,0 +1,7 @@
+package Loops;
+import java.util.Scanner;
+public class Task02SumOfMultilplyOf4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
