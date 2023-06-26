a09cb6a90b9ad93cad36146d0c3c35da8d469447
kenanevni <kenanevni@gmail.com>
2023-06-26 11:39:15 +0300

    0 neutral(ineffective) element in addition

--- java101/src/Loops/SumOfOddNumbers.java
+++ java101/src/Loops/SumOfOddNumbers.java
@@ -1,4 +1,14 @@
 package Loops;
+/*We write a program that accepts input from the user until a negative value is entered and adds the odd numbers from the entered values and prints it on the screen.*/
 
+import java.util.Scanner;
 public class SumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n;
+        int total = 0;//default value  0 ineffective element
+        do {
+            n = scan.nextInt();
+        }while (n>0)
+    }
 }
