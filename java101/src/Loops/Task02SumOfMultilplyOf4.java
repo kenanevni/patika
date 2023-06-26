718e9ceced383ee03bc6fcd60b6986bb68d835aa
kenanevni <kenanevni@gmail.com>
2023-06-26 14:44:26 +0300

    complted

--- java101/src/Loops/Task02SumOfMultilplyOf4.java
+++ java101/src/Loops/Task02SumOfMultilplyOf4.java
@@ -3,5 +3,16 @@ import java.util.Scanner;
 public class Task02SumOfMultilplyOf4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int n,sum = 0;
+
+        do {
+            n = scan.nextInt();
+            if (n % 4 == 0) {
+                sum += n;
+            }
+        }
+
+        while (n % 2 == 0);
+        System.out.println("Sum of Divisible 4 numbers " +sum);
     }
 }
