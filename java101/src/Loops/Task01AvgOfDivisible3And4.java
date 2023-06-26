5389acbdbf3f93590c482717fc3fe06f7ab17106
kenanevni <kenanevni@gmail.com>
2023-06-26 09:26:25 +0300

    Scanner imported

---
+++ java101/src/Loops/Task01AvgOfDivisible3And4.java
@@ -0,0 +1,13 @@
+package Loops;
+/*
+* Write a program that calculates the average of numbers divisible by 3 and 4 from the numbers 0
+* through the number entered.
+* */
+import java.util.Scanner;
+
+public class Task01AvgOfDivisible3And4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+
+}
