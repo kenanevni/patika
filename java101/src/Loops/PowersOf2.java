d5ea823a6ada12690a7629a0e9249d9b4bad3113
kenanevni <kenanevni@gmail.com>
2023-06-27 06:07:30 +0300

    variabes taken from user

--- java101/src/Loops/PowersOf2.java
+++ java101/src/Loops/PowersOf2.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class PowersOf2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Lets learn Powers of 2\n Please enter base number");
+        int baseNumber = scan.nextInt();
+        System.out.println("Please enter limit");
+        int limit = scan.nextInt();
 
 
         }
