f7a956820f1be9dbe384e522c88edbd1ceb2eec4
kenanevni <kenanevni@gmail.com>
2023-06-27 06:29:47 +0300

    initial commit

---
+++ java101/src/Loops/PowerOfNumbersToLimit.java
@@ -0,0 +1,10 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class PowerOfNumbersToLimit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("This program aim to print Powers of from 2 to given numbers till reach limit");
+    }
+}
