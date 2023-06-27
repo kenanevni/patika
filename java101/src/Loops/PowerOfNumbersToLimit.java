510ffc93d1ec9cbd9259a64d12bd70c826f6e333
kenanevni <kenanevni@gmail.com>
2023-06-27 06:31:18 +0300

    variables taken

--- java101/src/Loops/PowerOfNumbersToLimit.java
+++ java101/src/Loops/PowerOfNumbersToLimit.java
@@ -6,5 +6,8 @@ public class PowerOfNumbersToLimit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("This program aim to print Powers of from 2 to given numbers till reach limit");
+        System.out.println("Please enter base number");
+        int baseNumber = scanner.nextInt();
+        System.out.println("Please enter limit");
     }
 }
