bb596cdd002c1a8d57be29dc6c1cacb11769de0b
kenanevni <kenanevni@gmail.com>
2023-06-27 21:50:07 +0300

    completed

--- java101/src/Loops/NFactorial.java
+++ java101/src/Loops/NFactorial.java
@@ -1,10 +1,11 @@
 package Loops;
+
 import java.util.Scanner;
+
 public class NFactorial {
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a positive integer ");
-
         int n = scanner.nextInt();
 
     }
