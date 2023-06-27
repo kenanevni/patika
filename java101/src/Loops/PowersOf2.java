d658dd0b5ce9793ca890516db7462df9e1ff8418
kenanevni <kenanevni@gmail.com>
2023-06-27 06:06:45 +0300

    initial commit

---
+++ java101/src/Loops/PowersOf2.java
@@ -0,0 +1,10 @@
+package Loops;
+import java.util.Scanner;
+public class PowersOf2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        }
+    }
+}
