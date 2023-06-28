64c15d5db388a1de47f1bf4eac915ad98cb981be
kenanevni <kenanevni@gmail.com>
2023-06-28 16:05:21 +0300

    description of variables

---
+++ java101/src/Loops/PowersOfN.java
@@ -0,0 +1,15 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class PowersOfN {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lets learn n to  r Powers \n Please enter number n");
+        int n = scan.nextInt();
+        System.out.println("Please enter r");
+        int r = scan.nextInt();
+        int result = 1;
+
+    }
+}
