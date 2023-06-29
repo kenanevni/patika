d64221298c4607989088d6211359a5d368a36e44
kenanevni <kenanevni@gmail.com>
2023-06-29 23:54:38 +0300

    renamed

--- java101/src/Loops/IsArmstrong.java
+++ java101/src/Loops/IsArmstrong.java
@@ -7,13 +7,21 @@ public class Armstrong {
         Scanner scan = new Scanner(System.in);
         int input = scan.nextInt();
         int digitNumberCounter = 0;
+        int tempInput = input;
         //let's find digit-number of input
         while (input != 0) {
             input /= 10;
             digitNumberCounter++;
         }
+        System.out.println("tempInput = " + tempInput);
+        System.out.println("input = " + input);
         System.out.println("digitNumberCounter = " + digitNumberCounter);
+        //let's find numeric values of input
+        while (tempInput != 0) {
+            tempInput %=10;
+        }
 
 
+        System.out.println("tempInput2 = " + tempInput);
     }
 }
