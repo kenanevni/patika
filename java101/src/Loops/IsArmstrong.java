551a6a1db376fed94d162afba138fb3f5565e625
kenanevni <kenanevni@gmail.com>
2023-07-03 08:23:33 +0300

    completed

--- java101/src/Loops/IsArmstrong.java
+++ java101/src/Loops/IsArmstrong.java
@@ -8,20 +8,35 @@ public class IsArmstrong {
         int input = scan.nextInt();
         int digitNumberCounter = 0;
         int tempInput = input;
+        int numericValueOfInput;
+        int digitNumberPow;
+        int result = 0;
         //let's find digit-number of input
-        while (input != 0) {
-            input /= 10;
-            digitNumberCounter++;
-        }
-        System.out.println("tempInput = " + tempInput);
-        System.out.println("input = " + input);
-        System.out.println("digitNumberCounter = " + digitNumberCounter);
-        //let's find numeric values of input
         while (tempInput != 0) {
-            tempInput %=10;
+            tempInput /= 10;
+            digitNumberCounter++;
+        }//because of whlile loop tempInput = 0
+
+        //let's find numeric values of input
+        tempInput = input;
+        while (tempInput != 0) {
+            numericValueOfInput = tempInput % 10;
+            digitNumberPow = 1;
+            for (int i = 0; i < digitNumberCounter; i++) {
+                digitNumberPow *= numericValueOfInput;
+            }
+            result += digitNumberPow;
+            tempInput /= 10;
+            System.out.println("numericValueOfInput = " + numericValueOfInput);
+            System.out.println("digitNumberPow = " + digitNumberPow);
+            System.out.println("sub total = " + result);
         }
 
 
-        System.out.println("tempInput2 = " + tempInput);
+        if (result == input) {
+            System.out.println("Input number is " + input + "\nResult is " + result + "\nThis is a Armstrong number");
+        } else
+            System.err.println("Input number is " + input + "\nResult is " + result + "\nThis is NOT a Armstrong number");
+
     }
 }
