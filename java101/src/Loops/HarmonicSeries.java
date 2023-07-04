e4858b6a44635aada9632537a82bcc99a6994c46
kenanevni <kenanevni@gmail.com>
2023-07-04 12:33:19 +0300

    added harmonicSeries method

--- java101/src/Loops/HarmonicSeries.java
+++ java101/src/Loops/HarmonicSeries.java
@@ -10,6 +10,17 @@ public class HarmonicSeries {
         System.out.println("Here is the harmonic series and sum of it");
         System.out.println(harmonicSeries(number));
     }
+    public static double harmonicSeries(double number) {
+        double sum = 0;
+        for (double i = 1; i <= number; i++) {
+            double harmonicNumber = 1 / i;
+            sum += 1 / i;
+            //System.out.print(String.format("%.2f",harmonicNumber)+ "+");
+            System.out.println("1/" + i);
+        }
 
+        return sum;
+
+    }
 
 }
