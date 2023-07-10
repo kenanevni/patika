96da4ebc18ee4904983b6322e6accd43db330c1a
kenanevni <kenanevni@gmail.com>
2023-07-10 10:12:33 +0300

    scanner and variables added

---
+++ java101/src/Loops/LCMAndHCFWithForLoop.java
@@ -0,0 +1,16 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class LCMAndHCFWithForLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Highest Common Factor and Least Common Multiple Finder");
+        System.out.println("Please enter two integer");
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+    }
+
+
+}
