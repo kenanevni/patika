b6ce3cfef213a49d3193349cbc694e0d12a7daf0
kenanevni <kenanevni@gmail.com>
2023-07-10 11:26:19 +0300

    mn max values -2147483648 & 2147483647

--- java101/src/Loops/MinMaxNumber.java
+++ java101/src/Loops/MinMaxNumber.java
@@ -1,7 +1,28 @@
 package Loops;
+
 import java.util.Scanner;
+
 public class MinMaxNumber {
     public static void main(String[] args) {
+        int min=2147483647;
+        int max = -2147483648 ;
+        int numbers;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Min and Max Number Finder");
+        System.out.println("Please enter how many number would you like to compare");
+        int count = input.nextInt();
+        for (int i = 0; i < count; i++) {
+            numbers = input.nextInt();
 
+            if (numbers > max) {
+                max = numbers;
+            }
+            if (numbers < min) {
+                min = numbers;
+            }
+
+        }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
     }
 }
