e689cc02249e4c8ffaa7755507aeb3957c60ea76
kenanevni <kenanevni@gmail.com>
2023-07-11 11:37:48 +0300

    completed

---
+++ java101/src/Loops/PrimeNumbers.java
@@ -0,0 +1,23 @@
+package Loops;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int count = 0;
+        int temp=0;
+        for (int i = 2; i <= 100; i++) {
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.println("i = " + i);
+
+            }
+            count=temp;
+        }
+
+
+    }
+}
+
