e499fdf6642341d9e8d6ab94d3a49a03c1f38a15
kenanevni <kenanevni@gmail.com>
2023-07-11 15:55:15 +0300

    mutiplicationTable

---
+++ java101/src/Loops/HackerRankMultipliers.java
@@ -0,0 +1,22 @@
+package Loops;
+import java.util.Scanner;
+public class HackerRankMultipliers {
+    public static void main(String[] args) {
+        mutiplicationTable();
+
+    }
+
+    public static void mutiplicationTable() {
+        System.out.println("Please enter a number between 2 and 20");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        if (n >= 2 && n <= 20) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(n +" x "+ i +" = "+ (n * i));
+
+            }
+        }else System.err.println("Please enter a number between 2 and 20");
+
+    }
+}
