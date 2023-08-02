bc405c9de6de8c49121d4a04ee021cc270e6cd38
kenanevni <kenanevni@gmail.com>
2023-08-02 15:10:33 +0300

    User entered values to defined dimesnsions

---
+++ java101/src/Arrays/Matrix1.java
@@ -0,0 +1,24 @@
+package Arrays;
+import java.util.Scanner;
+public class Matrix1 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        Scanner input = new Scanner(System.in);
+        System.out.println("This is matrix that contain 2 row and two column Please enter matrix values");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length ; j++) {
+                matrix[i][j]=input.nextInt();
+            }
+
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length ; j++) {
+                System.out.print(matrix[i][j]+ " ");
+            }
+            System.out.println("");
+
+        }
+
+
+    }
+}
