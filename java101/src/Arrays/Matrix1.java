0c741bdb46e4513f55af6368b81894771def8479
kenanevni <kenanevni@gmail.com>
2023-08-02 18:07:42 +0300

    printMatrix method used

--- java101/src/Arrays/Matrix1.java
+++ java101/src/Arrays/Matrix1.java
@@ -1,7 +1,12 @@
 package Arrays;
 import java.util.Scanner;
+
+import static Arrays.PrintMatrix.printMatrix;
+
 public class Matrix1 {
     public static void main(String[] args) {
+        PrintMatrix printMatrix = new PrintMatrix();
+
         int[][] matrix = new int[2][3];
         Scanner input = new Scanner(System.in);
         System.out.println("This is matrix that contain 2 row and two column Please enter matrix values");
@@ -11,14 +16,19 @@ public class Matrix1 {
             }
 
         }
-        for (int i = 0; i < matrix.length; i++) {
+  /*      for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length ; j++) {
                 System.out.print(matrix[i][j]+ " ");
             }
             System.out.println("");
 
-        }
+        }*/
+        printMatrix(matrix);
+
 
 
     }
+
+
+
 }
