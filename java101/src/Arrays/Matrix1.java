821c5b7bd82004e0df3ec46d69b95ef9a2a70169
kenanevni <kenanevni@gmail.com>
2023-08-02 15:30:41 +0300

    completed

--- java101/src/Arrays/Matrix1.java
+++ java101/src/Arrays/Matrix1.java
@@ -2,17 +2,17 @@ package Arrays;
 import java.util.Scanner;
 public class Matrix1 {
     public static void main(String[] args) {
-        int[][] matrix = new int[3][3];
+        int[][] matrix = new int[2][3];
         Scanner input = new Scanner(System.in);
         System.out.println("This is matrix that contain 2 row and two column Please enter matrix values");
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length ; j++) {
+            for (int j = 0; j < matrix[i].length ; j++) {
                 matrix[i][j]=input.nextInt();
             }
 
         }
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length ; j++) {
+            for (int j = 0; j < matrix[i].length ; j++) {
                 System.out.print(matrix[i][j]+ " ");
             }
             System.out.println("");
