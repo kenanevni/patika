94ba2a05917d3551778efb46610733e331f8a4a6
kenanevni <kenanevni@gmail.com>
2023-08-05 19:27:07 +0300

    ok

--- java101/src/Arrays/PrintMatrix.java
+++ java101/src/Arrays/PrintMatrix.java
@@ -1,7 +1,7 @@
 package Arrays;
 
 public class PrintMatrix {
-    public  static void printMatrix(int[][] matrix) {
+    public static void printMatrix(int[][] matrix) {
         for (int row = 0; row < matrix.length; row++) {
             for (int column = 0; column < matrix[row].length ; column++) {
                 System.out.print(matrix[row][column]+ " ");
