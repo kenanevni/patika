17ac52d9cba37edce56bbb20fefc0c0e244a7dfc
kenanevni <kenanevni@gmail.com>
2023-08-06 18:58:39 +0300

    completed

--- java101/src/Arrays/DuplicatedNumbers.java
+++ java101/src/Arrays/DuplicatedNumbers.java
@@ -2,13 +2,13 @@ package Arrays;
 
 public class DuplicatedNumbers {
     public static void main(String[] args) {
-        int[] list = {1, 2, 3,1,2,3,4,5,};
+        int[] list = {1, 2, 3,1,2,3,4,5,5};
         duplicatedNumbers(list);
 
     }
 
     static void duplicatedNumbers(int[] array) {
-        int count=0;
+
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j]) {
